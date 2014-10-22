eb67e804d7e97053d8bc76ea8e5716ea689ba141
vitalys <pumych@gmail.com>
2014-10-22 17:35:39 +0300

    xz

--- models/db.js
+++ models/db.js
@@ -12,14 +12,38 @@ var MainSchema = mongoose.Schema({
     }
 });
 
-//var MainModel = mongoose.model('sisk', MainSchema);
-
 MainSchema.statics.getPosts = function(req, res){
     this.model('post').find({}, function(err, data){
         if(err) console.log(err);
         console.log(data);
         res.end();
     });
+}
+
+var PostSchema = mongoose.Schema({
+        img_url:    String,
+        title:      String,
+        content:    String
+});
+
+var Post = mongoose.model('post', PostSchema);
+
+//var MainModel = mongoose.model('sisk', MainSchema);
+
+
+
+MainSchema.statics.addPost = function(req, res){
+
+
+    var newPost = new MainSchema({ post: {img_url: req.img_url, title: req.title, content: req.content}});
+    var M = this.model('M', newPost);
+    var m = new M()
+
+    this.model('post').save( function(){
+
+    });
+
+    res.end();
 }
 
 module.exports = mongoose.model('post', MainSchema);
